c358c5658f547566ef83afbf18ef858941ee9d7f
Leonn <leonwangchn@163.com>
2018-06-09 21:37:06 +0800

    modify data

--- Classes/Data/UnitData.h
+++ Classes/Data/UnitData.h
@@ -24,16 +24,16 @@ namespace unitData
     
     namespace EnoughPower
     {
-        const int infantryWait = 5 * 1000;
-        const int dogWait = 5 * 1000;
-        const int tankWait = 10 * 1000;
+        const int infantryWait = 3 * 1000;
+        const int dogWait = 3 * 1000;
+        const int tankWait = 6 * 1000;
     }
     
     namespace NotEnoughPower
     {
-        const int infantryWait = 10 * 1000;
-        const int dogWait = 10 * 1000;
-        const int tankWait = 20 * 1000;
+        const int infantryWait = 6 * 1000;
+        const int dogWait = 6 * 1000;
+        const int tankWait = 12 * 1000;
     }
 }
 
